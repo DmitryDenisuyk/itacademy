4566231682a704fbfb561062cd3968d39d45e6c4
DmitryDenisuyk <dmitrijddm@gmail.com>
2021-10-01 22:57:18 +0300

    task2

---
+++ home work L5/task2.js
@@ -0,0 +1,21 @@
+function num13thFri(mass){
+    let count = 0;
+    for (let i=0; i < mass.length; i++){
+    let year = mass[i]; 
+    for (let month = 0; month<12; month++) {
+        let st3 = new Date(year, month,13);
+       if(st3.getDay() == 5){
+        count++;
+     }
+    }
+}
+    return count;                            
+}
+
+let ffgdgdfgdgf = [];
+
+for (let i = 2000; i < new Date().getFullYear(); i++){
+    ffgdgdfgdgf.push(i);
+} 
+
+console.log(num13thFri(ffgdgdfgdgf)); 
